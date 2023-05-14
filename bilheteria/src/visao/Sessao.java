2bc7e0ad55fc0bbfcee92e07bdc81f9998e2704b
Leticia Lima <leticia.lc2005@aluno.ifsc.edu.br>
2023-05-14 18:02:12 -0300

    Tela Sessao inicio

--- bilheteria/src/visao/Sessao.java
+++ bilheteria/src/visao/Sessao.java
@@ -5,6 +5,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Panel;
+import java.awt.Toolkit;
+
+import javax.swing.UIManager;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.ImageIcon;
 
 public class Sessao extends JFrame {
 
@@ -31,11 +43,42 @@ public class Sessao extends JFrame {
 	 */
 	public Sessao() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		this.setExtendedState(MAXIMIZED_BOTH);
+		setBounds(100, 100, 1450, 1300);
 		contentPane = new JPanel();
+		contentPane.setBounds(100, 100, 450, 300);
+		contentPane.setBackground(new Color(69, 159, 175));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 29, 1400, 125);
+		panel.setBackground(UIManager.getColor("Button.highlight"));
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblSessao = new JLabel("SESSÃO");
+		lblSessao.setBounds(566, 25, 268, 74);
+		lblSessao.setVerticalAlignment(SwingConstants.BOTTOM);
+		lblSessao.setFont(new Font("Verdana", Font.BOLD, 60));
+		lblSessao.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lblSessao);
+		
+		JLabel lblGuardioes = new JLabel("Guardiões da Galáxia");
+		lblGuardioes.setBounds(67, 192, 179, 14);
+		lblGuardioes.setFont(new Font("Verdana", Font.BOLD, 15));
+		contentPane.add(lblGuardioes);
+		
+		JLabel lblShrek = new JLabel("Shrek");
+		lblShrek.setBounds(67, 367, 129, 14);
+		lblShrek.setFont(new Font("Verdana", Font.BOLD, 15));
+		contentPane.add(lblShrek);
+		
+		JLabel lblNewLabel_1 = new JLabel("Barbie");
+		lblNewLabel_1.setFont(new Font("Verdana", Font.BOLD, 15));
+		lblNewLabel_1.setBounds(67, 525, 61, 14);
+		contentPane.add(lblNewLabel_1);
 	}
-
 }
